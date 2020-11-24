42fa77d070079b29381e8f9d8e2a1e43daf9f9e1
Matt Sirkis <sirkis.matt@gmail.com>
2020-11-24 13:17:14 +1300

    added Profile and steps to deploy to heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
-const app = require('./app')
-const { PORT } = require('./config')
+const express = require('express');
+const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
-})
+const PORT = process.env.PORT || 8000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
